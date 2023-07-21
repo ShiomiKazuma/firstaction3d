37d3387712d879615f16ca0850e98f221940b66e
ShiomiKazuma <134474097+ShiomiKazuma@users.noreply.github.com>
2023-07-21 20:56:51 +0900

    [add]チャージショットの処理を追加

--- Assets/Script/PlayerShot.cs
+++ Assets/Script/PlayerShot.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,13 +8,47 @@ public class PlayerShot : MonoBehaviour
 {
     [SerializeField] GameObject _bullet;
     [SerializeField] GameObject _muzzle;
+    bool _bulletButtonDown = false;
+    public float _bulletCharge;
+    [SerializeField] float _bulletChargeMax = 100.0f;
+
+    //public float BulletCharge
+    //{
+    //    get { return _bulletCharge; }
+
+    //    private set
+    //    {
+    //        _bulletCharge = value;
+    //    }
+    //}
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetButtonDown("Fire1"))
         {
+            
+            _bulletButtonDown =true;
+        }
+
+        if(_bulletButtonDown)
+        {
+            _bulletCharge += 5.0f;
+
+            if(_bulletCharge > _bulletChargeMax)
+            {
+                _bulletCharge = _bulletChargeMax;
+            }
+
+        }
+
+        if(Input.GetButtonUp("Fire1"))
+        {
             Instantiate(_bullet, _muzzle.transform.position, _muzzle.transform.rotation);
+            _bulletButtonDown = false;
+            _bulletCharge = 0.0f;
         }
     }
+
+    
 }
